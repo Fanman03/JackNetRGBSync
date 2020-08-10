6f7780c8a5d533c71b6acbbd74175f5e39984969
Jack L.R. Pendleton <jacklrpendleton@gmail.com>
2020-08-10 16:41:27 -0500

    Icon improvements

--- RGBSync+/Converter/DeviceTypeToImageConverter.cs
+++ RGBSync+/Converter/DeviceTypeToImageConverter.cs
@@ -18,8 +18,22 @@ namespace RGBSyncPlus.Converter
             var type = (RGBDeviceType)value;
             switch (type)
             {
+                case RGBDeviceType.Keyboard:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Keyboard.png", UriKind.Absolute));
+                case RGBDeviceType.Mouse:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Mouse.png", UriKind.Absolute));
+                case RGBDeviceType.Headset:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Headphones.png", UriKind.Absolute));
+                case RGBDeviceType.Fan:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Fan.png", UriKind.Absolute));
+                case RGBDeviceType.LedStripe:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/LedStrip.png", UriKind.Absolute));
                 case RGBDeviceType.DRAM:
                     return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/DRAM.png", UriKind.Absolute));
+                case RGBDeviceType.Mainboard:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Motherboard.png", UriKind.Absolute));
+                case RGBDeviceType.GraphicsCard:
+                    return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/GPU.png", UriKind.Absolute));
                 default:
                     return new BitmapImage(new Uri("pack://application:,,,/RGBSync+;component/Resources/DevImg/Unknown.png", UriKind.Absolute));
             }
